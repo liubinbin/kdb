ab000290c57761440b8d89b636c682746d35b8d7
liubinbin <liubinbean@163.com>
2024-08-23 16:05:21 +0800

    增加 next，比较清晰。

--- src/main/java/cn/liubinbin/kdb/server/executor/AbstrExePlan.java
+++ src/main/java/cn/liubinbin/kdb/server/executor/AbstrExePlan.java
@@ -3,6 +3,7 @@ package cn.liubinbin.kdb.server.executor;
 public abstract class AbstrExePlan {
 
     public int kind;
+    public AbstrExePlan next;
 
     public AbstrExePlan(int kind) {
         this.kind = kind;
