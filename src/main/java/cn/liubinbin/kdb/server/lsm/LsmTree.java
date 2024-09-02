218cf6705c7eb868ceacdd021e0bc21d672bbd09
liubinbin <liubinbean@163.com>
2024-09-02 17:06:32 +0800

    提交lsm的实现和单测，lsm使用了ConcurrentSkipListMap实现，为自己实现。

--- src/main/java/cn/liubinbin/kdb/server/lsm/LsmTree.java
+++ src/main/java/cn/liubinbin/kdb/server/lsm/LsmTree.java
@@ -1,12 +1,18 @@
 package cn.liubinbin.kdb.server.lsm;
 
 import cn.liubinbin.kdb.server.entity.KdbRow;
+import cn.liubinbin.kdb.server.entity.KdbRowValue;
 import cn.liubinbin.kdb.server.executor.Engine;
+import cn.liubinbin.kdb.server.table.ColumnType;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * @author liubinbin
+ * @date 2024/09/02
  */
 public class LsmTree extends Engine {
 
@@ -14,5 +20,35 @@ public class LsmTree extends Engine {
 
     public LsmTree() {
         super();
+        this.tree = new ConcurrentSkipListMap<>();
     }
+
+    public void insert(KdbRow rowToInsert) {
+        tree.put(rowToInsert.getRowKey(), rowToInsert);
+    }
+
+    public void delete(KdbRow rowToInsert) {
+        tree.remove(rowToInsert.getRowKey());
+    }
+
+    public List<KdbRow> rangeScan(Integer lowerBound, Integer upperBound) {
+        return new ArrayList<>(tree.subMap(lowerBound, upperBound).values());
+    }
+
+
+    public static void main(String[] args) {
+        LsmTree lsmTree = new LsmTree();
+
+        for(int i = 1; i <= 6; i++) {
+            KdbRow curRow = new KdbRow(Collections.singletonList(new KdbRowValue(ColumnType.INTEGER, i)));
+            lsmTree.insert(curRow);
+        }
+
+        List<KdbRow> rows = lsmTree.rangeScan(4, 6);
+        System.out.println("rows size " + rows.size());
+        for (KdbRow row : rows) {
+            System.out.print(row.getRowKey() + ", ");
+        }
+    }
+
 }
--- src/test/java/cn/liubinbin/kdb/sever/lsm/LsmTest.java
+++ src/test/java/cn/liubinbin/kdb/sever/lsm/LsmTest.java
@@ -1,8 +1,15 @@
 package cn.liubinbin.kdb.sever.lsm;
 
+import cn.liubinbin.kdb.server.entity.KdbRow;
+import cn.liubinbin.kdb.server.entity.KdbRowValue;
+import cn.liubinbin.kdb.server.lsm.LsmTree;
+import cn.liubinbin.kdb.server.table.ColumnType;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author liubinbin
@@ -14,6 +21,18 @@ public class LsmTest {
      */
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+        LsmTree lsmTree = new LsmTree();
+
+        for(int i = 1; i <= 6; i++) {
+            KdbRow curRow = new KdbRow(Collections.singletonList(new KdbRowValue(ColumnType.INTEGER, i)));
+            lsmTree.insert(curRow);
+        }
+
+        List<KdbRow> rows = lsmTree.rangeScan(Integer.MIN_VALUE, Integer.MAX_VALUE);
+        assertEquals(6, rows.size());
+
+        rows = lsmTree.rangeScan(4, 6);
+
+        assertEquals(4, rows.get(0).getRowKey().longValue());
     }
 }
